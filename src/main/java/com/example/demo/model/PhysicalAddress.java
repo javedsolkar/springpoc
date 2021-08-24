b1a1b9fb93b3279b90c5f9085fb90adc5a4dce26
Your Name <you@example.com>
2021-08-24 11:28:00 +0530

    1st commit

--- src/main/java/com/example/demo/model/PhysicalAddress.java
+++ src/main/java/com/example/demo/model/PhysicalAddress.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "mmst_phyaddress")
@@ -9,6 +11,7 @@ public class PhysicalAddress {
 
 	@Column(name = "mmst_phyaddress_id")
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String id;
 
 	@Column(name = "sname")
--- src/main/java/com/example/demo/repo/PhysicalAddressRepo.java
+++ src/main/java/com/example/demo/repo/PhysicalAddressRepo.java
@@ -13,7 +13,6 @@ public class PhysicalAddressRepo {
 	PhysicalAddressJpaRepo physicalAddressJpaRepo;
 
 	public Iterable<PhysicalAddress> getAllPhysicalAddress() {
-
 		return physicalAddressJpaRepo.findAll();
 	}
 
